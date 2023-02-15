f93569dfa177c102d06d6cfe0e51a7aee4993bc4
Your Name <you@example.com>
2023-02-15 10:51:44 +0300

    odd-even

--- Week-2/for loop.py
+++ Week-2/for loop.py
@@ -1,2 +1,15 @@
+#print sum of first 10 numbers
+
+sum = 0
 for i in range (0,10):
-    print(1)
+    print(i)
+    sum = sum + i
+    print(sum) 
+ 
+ #print product of first 10 numbers
+
+    product= 1
+for u in range (1,10):
+    product= product * u 
+    print(product)
+ 
