6d84f4a438ed2e6fea24d413871a584fa8376777
Your Name <you@example.com>
2023-02-17 11:00:09 +0300

    Stringd

---
+++ Week-2/string.py
@@ -0,0 +1,23 @@
+#/user/bin/env/python3
+
+
+#This is a single line comment\
+#Pyhton programme illusttreates strings
+#Name : Bonface Kerosi 
+#Email : kerosionc@gmail.com
+#Date : 17th feb 2023
+#File: strings.py
+
+poem = """This poem is abount nothing
+     Its funny and people laugh about it """
+
+f_name= "Bonface"
+s_name ="Kerosi"
+full_name = f_name + s_name
+age = 25 #years
+
+print("My name is" +full_name+"and I am" +str(age)+"years old")
+print("My name is {}and I am {}years old".format(full_name,age))
+
+print("Hello from bonface \n how are you \n I am fine")
+print("Hello from bonface \n how are you \n I am fine")
