abf56fffc06c7f78734cbcffd9cdfcbb352c31f9
Your Name <you@example.com>
2023-02-20 09:58:57 +0300

    list_revised

---
+++ Week-3/list_revised.py
@@ -0,0 +1,21 @@
+#!/user/bin/env python3
+
+myFavorite_Fruits = ["Mango","banana","kiwi","pawpaw","Pineapples"]
+for Fruit in myFavorite_Fruits:
+    print(Fruit)
+print(len(Fruit))
+
+Friends = ["vic","Lem","brr","Fri","Sam"]
+print(Friends)
+Friends[0]= "Hunter"
+print(Friends)
+
+
+new_Friends = Friends.copy()
+print(new_Friends)
+
+new_Friends.sort()
+print(new_Friends)
+
+new_Friends.pop()
+print(new_Friends)
