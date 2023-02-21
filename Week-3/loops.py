8556d5e1c4a4e7f6f9a3842acfbd26886b543767
Your Name <you@example.com>
2023-02-21 10:37:00 +0300

    loops

---
+++ Week-3/loops.py
@@ -0,0 +1,17 @@
+#do while 
+while(True):
+    print("My name is Kay")
+    break
+    num = 74 
+    while(True):
+        if( num > 20):
+          print(num)
+          break
+
+age = 24
+gender = "male"
+weight = 80
+height = 130
+if(age > 18)&(gender =="male"):
+    if(weight > 75 | height < 125):
+        print("You are obesse")
