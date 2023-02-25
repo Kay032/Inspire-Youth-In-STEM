a78fe769a7dfe9269ec2e88955b3485dc3aa96f8
Your Name <you@example.com>
2023-02-25 17:41:17 +0300

    tests

--- Week-3/assignment.py
+++ Week-3/assignment.py
@@ -1,2 +1,13 @@
-#Write a program to print the factorial of a number using functions
+
 #Write a program to calculate simple interest 
+
+def simple_interest(principal, rate, time):
+
+    return (principal * rate * time) / 100
+
+p = 100000.0
+r = 8.5
+t = 3.0
+interest = simple_interest(p, r, t)
+print("Simple interest is:", interest)
+
